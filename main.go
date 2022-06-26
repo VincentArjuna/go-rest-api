5812bbe3ce51af6ae2adb2f85e38a3e914ee9d4e
Arjunanda <vincentiusmandala@gmail.com>
2022-06-26 09:31:19 +0700

    Insert Book

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"net/http"
 	"github.com/gin-gonic/gin"
-	"errors"
+	//"errors"
 )
 
 type book struct{
@@ -23,8 +23,23 @@ func getBooks(c *gin.Context){
 	c.IndentedJSON(http.StatusOK, books)
 }
 
+func createBooks(c *gin.Context){
+	var newBook book
+
+	if err:= c.BindJSON(&newBook); err != nil {
+		return
+	}
+
+	books = append(books, newBook)
+	c.IndentedJSON(http.StatusCreated, newBook)
+
+}
+
 func main() {
 	router := gin.Default()
 	router.GET("/books", getBooks)
+	//curl localhost:8080/books
+	router.POST("/books", createBooks)
+	//curl localhost:8080/books --include --header "Content-Type: application/json" -d @body.json --request "POST"
 	router.Run("localhost:8080")
 }
